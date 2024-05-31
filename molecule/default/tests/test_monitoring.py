ed823ed9841c5c7d9b810e0e32ec965a788858d2
Benjamin Schubert <contact@benschubert.me>
2024-05-31 07:34:38 +0100

    Make the grafana agent monitor the other parts of the monitoring stack

--- molecule/default/tests/test_monitoring.py
+++ molecule/default/tests/test_monitoring.py
@@ -57,4 +57,32 @@ def test_services_are_up(
     }
     assert entries == {
         ("agent", "1"),
+        ("grafana", "1"),
+        ("mimir", "1"),
+        ("postgresql://monitoring-grafana-postgres:5432/grafana", "1"),
+    }
+
+
+def test_postgres_databases_are_up(
+    hostvars: dict[str, Any],
+    session: requests.Session,
+    authentik_credentials: tuple[str, str],
+) -> None:
+    resp = session.get(
+        f"https://{hostvars['monitoring_mimir_hostname']}/prometheus/api/v1/query",
+        params={"query": "pg_up"},
+        allow_redirects=False,
+        auth=authentik_credentials,
+        timeout=10,
+    )
+    print(resp.text)
+    assert resp.status_code == HTTPStatus.OK
+    result = resp.json()
+    assert result["status"] == "success"
+    entries = {
+        (r["metric"]["instance"], r["value"][1])
+        for r in result["data"]["result"]
+    }
+    assert entries == {
+        ("postgresql://monitoring-grafana-postgres:5432/grafana", "1"),
     }
