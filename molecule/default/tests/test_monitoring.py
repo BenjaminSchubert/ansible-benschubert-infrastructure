8924650fee2d0a884e8456e00ffefa5158bda81f
Benjamin Schubert <contact@benschubert.me>
2024-06-03 08:36:54 +0100

    Add an agent deployment to monitor Authentik
    This also adds the ability to monitor correctly configured redis
    instances

--- molecule/default/tests/test_monitoring.py
+++ molecule/default/tests/test_monitoring.py
@@ -58,12 +58,27 @@ def test_services_are_up(
     assert entries == {
         (
             "agent",
+            "prometheus.scrape.benschubert_infrastructure_auth_monitor",
+            "1",
+        ),
+        (
+            "agent",
             "prometheus.scrape.benschubert_infrastructure_ingress_monitor",
             "1",
         ),
         (
             "agent",
             "prometheus.scrape.benschubert_infrastructure_monitoring_monitor",
+            "1",
+        ),
+        (
+            "auth-redis",
+            "integrations/redis",
+            "1",
+        ),
+        (
+            "authentik",
+            "prometheus.scrape.benschubert_infrastructure_auth_monitor",
             "1",
         ),
         (
@@ -74,6 +89,11 @@ def test_services_are_up(
         (
             "mimir",
             "prometheus.scrape.benschubert_infrastructure_monitoring_monitor",
+            "1",
+        ),
+        (
+            "postgresql://auth-postgres:5432/authentik",
+            "integrations/postgres",
             "1",
         ),
         (
@@ -111,8 +131,42 @@ def test_postgres_databases_are_up(
     }
     assert entries == {
         (
+            "postgresql://auth-postgres:5432/authentik",
+            "integrations/postgres",
+            "1",
+        ),
+        (
             "postgresql://monitoring-grafana-postgres:5432/grafana",
             "integrations/postgres",
             "1",
         ),
+    }
+
+
+def test_redis_deployments_are_up(
+    hostvars: dict[str, Any],
+    session: requests.Session,
+    authentik_credentials: tuple[str, str],
+) -> None:
+    resp = session.get(
+        f"https://{hostvars['monitoring_mimir_hostname']}/prometheus/api/v1/query",
+        params={"query": "redis_up"},
+        allow_redirects=False,
+        auth=authentik_credentials,
+        timeout=10,
+    )
+    print(resp.text)
+    assert resp.status_code == HTTPStatus.OK
+    result = resp.json()
+    assert result["status"] == "success"
+    entries = {
+        (r["metric"]["instance"], r["metric"]["job"], r["value"][1])
+        for r in result["data"]["result"]
+    }
+    assert entries == {
+        (
+            "auth-redis",
+            "integrations/redis",
+            "1",
+        )
     }
